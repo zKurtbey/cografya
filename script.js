259bf2e562b82f4078beceda86642524ac1ef2b5
zKurtbey <140351544+zKurtbey@users.noreply.github.com>
2024-01-07 00:38:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sorular = ["Aşağıdakilerden hangisi volkanik bir dağdır?", "Aşağıdakilerden hangisi yerkabuğunun en ince katmanıdır?", "Manto'nun sona erdiği, 2.900m ile yerin merkezi olan 6.300km arasında yer alan katman hangisidir?"];
 var siklar = ["Yunt Dağı", "Kazma Dağı", "Boz Dağ", "Aydın Dağı", "Ağrı Dağı", "İç Çekirdek", "Dış Çekirdek", "Manto", "Yer Kabuğu", "Atmosfer", "Sima", "Sial", "Gökyüzü", "Çekirdek", "Stratosfer"];
 const soru = sorular[Math.floor(Math.random() * sorular.length)];
-document.querySelector("#soruyazi").innerHTML=soru;
+document.querySelector("#sorubaslik").innerHTML=soru;
 const cevap = document.querySelectorAll(".cevap");
 cevap.forEach((cevap), () => {
   const suk = siklar[Math.floor(Math.random() * siklar.length)];
