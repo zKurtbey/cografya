c1b0ee8da70729a6a84bc8fb4b6567d31814b509
zKurtbey <140351544+zKurtbey@users.noreply.github.com>
2024-01-07 12:20:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const sorular = ["Aşağıdakilerden hangisi volkanik bir dağdır?", "Aşağıdakilerden hangisi yerkabuğunun en ince katmanıdır?", "Manto'nun sona erdiği, 2.900m ile yerin merkezi olan 6.300km arasında yer alan katman hangisidir?"];
+const sorular = ["Aşağıdakilerden hangisi volkanik bir dağdır?", "Aşağıdakilerden hangisi Dünya'nın en ince katmanıdır?", "Manto'nun sona erdiği, 2.900km ile yerin merkezi olan 6.300km arasında yer alan katman hangisidir?"];
 var siklar = ["Yunt Dağı", "Kazma Dağı", "Boz Dağ", "Aydın Dağı", "Ağrı Dağı", "İç Çekirdek", "Dış Çekirdek", "Manto", "Yer Kabuğu", "Atmosfer", "Sima", "Sial", "Gökyüzü", "Çekirdek", "Stratosfer"];
 const soru = sorular[Math.floor(Math.random() * sorular.length)];
 document.querySelector("#sorubaslik").innerHTML=soru;
-const cevap = document.querySelectorAll(".cevap");
 var suk;
-cevap.forEach((cevap) =>{
+var cvpsikki = [document.querySelectorAll(".cevap")[0], document.querySelectorAll(".cevap")[1], document.querySelectorAll(".cevap")[2], document.querySelectorAll(".cevap")[3], document.querySelectorAll(".cevap")[4]];
+if(soru.includes("volkanik" && "dağ")){
+  var dogrucvpsikki = [document.querySelectorAll(".cevap")[0], document.querySelectorAll(".cevap")[1], document.querySelectorAll(".cevap")[2], document.querySelectorAll(".cevap")[3], document.querySelectorAll(".cevap")[4]];
+  var dogrucvp = dogrucvpsikki[Math.floor(Math.random() * dogrucvpsikki.length)];
+  dogrucvp.innerHTML="Ağrı Dağı";
+  siklar = siklar - "Ağrı Dağı";
+  cvpsikki = cvpsikki - dogrucvpsikki;
+} else if(soru.includes("ince" && "katman")){
+  var dogrucvpsikki = [document.querySelectorAll(".cevap")[0], document.querySelectorAll(".cevap")[1], document.querySelectorAll(".cevap")[2], document.querySelectorAll(".cevap")[3], document.querySelectorAll(".cevap")[4]];
+  var dogrucvp = dogrucvpsikki[Math.floor(Math.random() * dogrucvpsikki.length)];
+  dogrucvp.innerHTML="Yer Kabuğu";
+  siklar = siklar - "Yer Kabuğu";
+  cvpsikki = cvpsikki - dogrucvpsikki;
+} else if(soru.includes("2.900km" && "6.300km")){
+  var dogrucvpsikki = [document.querySelectorAll(".cevap")[0], document.querySelectorAll(".cevap")[1], document.querySelectorAll(".cevap")[2], document.querySelectorAll(".cevap")[3], document.querySelectorAll(".cevap")[4]];
+  var dogrucvp = dogrucvpsikki[Math.floor(Math.random() * dogrucvpsikki.length)];
+  dogrucvp.innerHTML="Çekirdek";
+  siklar = siklar - "Çekirdek";
+  cvpsikki = cvpsikki - dogrucvpsikki;
+}
+cvpsikki.forEach((cvpsikki) =>{
   suk = siklar[Math.floor(Math.random() * siklar.length)];
   cevap.innerHTML = suk;
 });
