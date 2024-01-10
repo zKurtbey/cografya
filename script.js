bb601d7061d280fbd36bedccfd1be3ef1cc83a5c
zKurtbey <140351544+zKurtbey@users.noreply.github.com>
2024-01-10 23:51:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ var sahtesiklar = [
   var soru;
   var dogrucvp;
   const suklar = document.querySelectorAll(".suk");
-function soru(yzlnskkldr, yzlncvpkldr, grckcvp){
+function soru(yzlnskkldr, yzlncvpkldr, grckcvpdgr){
   soru = sorular[Math.floor(Math.random() * sorular.length)];
   document.querySelector("#sorubaslik").innerHTML=soru;
   sorukonumu = sorular.indexOf(soru);
@@ -209,7 +209,7 @@ function soru(yzlnskkldr, yzlncvpkldr, grckcvp){
     dogrucvp.parentNode.addEventListener("click", ()=>{
       dogrucvp.parentNode.style="backdrop-filter: blur(100px); background-color: rgb(3 205 10 / 20%);opacity: 1;";
       console.log("doğru");
-      soru(yzlnskkldr, yzlncvpkldr, grckcvp);
+      soru(yzlnskkldr, yzlncvpkldr, grckcvpdgr);
     });
   }
 }
